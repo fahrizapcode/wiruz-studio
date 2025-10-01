1f598bcdfa38bdf7c1663a9b7f9023c067f1197b
FahrizapCode <fahrizapcode>
2025-10-01 22:02:29 +0700

    Updates

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // static export
-  images: { unoptimized: true },
-  basePath: "/wiruz-studio", // pakai nama repo kamu
-};
-
-export default nextConfig;
