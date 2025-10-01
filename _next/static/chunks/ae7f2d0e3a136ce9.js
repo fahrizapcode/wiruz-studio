6feaebc3b3a8643941300a44a74a34805356bf4b
fahrizapcode <fahrizapcode@users.noreply.github.com>
2025-10-01 22:50:29 +0000

    deploy: 43192afefe10c9c4d46424c83f1c223c14420204

--- _next/static/chunks/ae7f2d0e3a136ce9.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/9e446a754779b67e.js",
-  "static/chunks/turbopack-b4b0f240370cf64d.js"
-])
--- _next/static/chunks/c6c9db70af9b4a0f.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/9e446a754779b67e.js",
-  "static/chunks/turbopack-278021f6141b0b51.js"
-])
