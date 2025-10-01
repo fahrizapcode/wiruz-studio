be0581e946b2a46255343f9b6a0c1259821a3b94
FahrizapCode <fahrizapcode>
2025-10-01 15:28:14 +0700

    Percobaan 15

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: "export", // static export
   images: { unoptimized: true },
   basePath: "/wiruz-studio", // pakai nama repo kamu
-  assetPrefix: "/wiruz-studio/",
 };
 
 export default nextConfig;
