a4ec8843ab5f04851e65c7e5b4827fd64951f502
FahrizapCode <fahrizapcode>
2025-10-01 12:35:53 +0700

    percobaan 1

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
-  basePath: "/wiruz-studio",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/wiruz-studio", // karena repo kamu bukan username.github.io
 };
 
 export default nextConfig;
